653ea0a51bdf5784d7671f774a3f44e0e06e84b8
Johan Larsson <johanlarsson@outlook.com>
2024-01-11 16:57:31 +0100

    build: rely on l3build instead

---
+++ build.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env texlua
+
+module = "moloch"
+
+-- sourcefiledir  = "src"
+--
+-- installfiles = {"*.sty"}
+-- sourcefiles  = {"src/*.dtx"}
+-- typesetfiles = {"src/*.dtx"}
+
+sourcefiledir = "src"
+-- docfiledir = "docs"
+-- unpackfiles = { "*.dtx" }
+-- -- typesetfiles = { "*.dtx", "*.tex" }
+-- typesetfiles = { "moloch.dtx" }
+-- installfiles = {"*.sty", "*.dtx"}
+-- packtdszip = true
+
+typesetfiles = {"doc/moloch.tex"}
+-- sourcefiles = {"src/*.dtx"}
+-- docfiles = {"doc/*.tex"}
