fb577ac821c0917b047b0afec0f9a78d3c715741
Johan Larsson <johanlarsson@outlook.com>
2024-01-23 20:33:10 +0100

    build: set up uploadconfig

--- build.lua
+++ build.lua
@@ -9,6 +9,24 @@ checksuppfiles = { "*.tex" }
 
 maxprintline = 9999
 
+uploadconfig = {
+  pkg = "moloch",
+  version = "0.1.0", -- x-release-please-version
+  author = "Johan Larsson",
+  license = "CC BY-SA 4.0",
+  summary = "A clean and simple beamer theme",
+  description = [[
+    Moloch is a clean and simple beamer theme. It is a fork of the
+    Metropolis theme, but has a more minimalistic and slightly less
+    opinionated design.
+  ]],
+  ctanPath = "/macros/latex/contrib/beamer-contrib/themes/moloch",
+  repository = "https://github.com/jolars/moloch",
+  bugtracker = "https://github.com/jolars/moloch/issues",
+  topic = "Presentation",
+  update = false,
+}
+
 function update_tag(file, content, tagname, tagdate)
   if string.match(file, "%.dtx$") then
     local pattern = "\\ProvidesPackage{(.-)}%[([^%]]-) v([^%]]-) ([^%]]-)%]"
